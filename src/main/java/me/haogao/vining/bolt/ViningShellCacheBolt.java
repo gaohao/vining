36bef3677993f8460253981dbdf85435a91a1149
Hao Gao <hao.x.gao@gmail.com>
2013-02-26 04:53:01 -0500

    fix bug shell topology

---
+++ src/main/java/me/haogao/vining/bolt/ViningShellCacheBolt.java
@@ -0,0 +1,65 @@
+package me.haogao.vining.bolt;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.TimeZone;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+
+public class ViningShellCacheBolt extends BaseRichBolt{
+
+	private static final long serialVersionUID = 5161826509198496313L;
+	private String host;
+	private int port;
+	private JedisPool jedisPool = null;
+	private Jedis jedis= null;
+	private static Logger logger;
+	
+	public ViningShellCacheBolt(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void prepare(Map stormConf, TopologyContext context,
+			OutputCollector collector) {
+		this.jedisPool = new JedisPool(this.host, this.port);
+		this.jedis = jedisPool.getResource();
+		logger = LogManager.getLogger(ViningShellCacheBolt.class.getName());
+	}
+
+	@Override
+	public void execute(Tuple input) {
+		String link = (String) input.getValue(0);
+		String key = "vine:link";
+		this.jedis.sadd(key, link);
+		//logger.info(link);
+	}
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		
+	}
+	
+	@Override
+	public void cleanup() {
+		if (this.jedisPool != null ) {
+			this.jedisPool.returnResource(this.jedis);
+			this.jedisPool.destroy();
+		}
+	}
+	
+}
+
---
+++ src/main/java/me/haogao/vining/topology/ViningShellTopology.java
@@ -0,0 +1,46 @@
+package me.haogao.vining.topology;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import me.haogao.vining.bolt.*;
+import me.haogao.vining.spout.*;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+
+public class ViningShellTopology {
+    
+    public static void main(String[] args) throws Exception {
+		Logger logger = LogManager.getLogger(ViningShellTopology.class.getName());
+    	logger.entry();
+    	
+    	
+        TopologyBuilder builder = new TopologyBuilder();
+        
+        builder.setSpout("spout", new ViningShellSpout(), 1);
+        
+        builder.setBolt("bolt", new ViningShellCacheBolt("localhost", 6379), 12)
+                 .fieldsGrouping("spout", new Fields("link"));
+
+        Config conf = new Config();
+        conf.setDebug(true);
+        
+        if(args!=null && args.length > 0) {
+            conf.setNumWorkers(3);
+            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        } else {        
+            conf.setMaxTaskParallelism(3);
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("vining", conf, builder.createTopology());
+            
+            Thread.sleep(100000);
+            cluster.killTopology("vining");
+            cluster.shutdown();
+        }  
+		logger.exit();
+    }
+}
