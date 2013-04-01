0e8cd69279cde788d370205968d9bfed0fa4d4b2
Di Li <lidise7en@gmail.com>
2013-04-01 03:04:40 -0300

    Update vining_tweepy_spout.py
    Add parameter(get from argv) to query

--- multilang/resources/vining_tweepy_spout.py
+++ multilang/resources/vining_tweepy_spout.py
@@ -16,7 +16,10 @@ class ViningSpout(storm.Spout):
     
     def nextTuple(self):
         try:       
-            for tweet in self.api.search(q ="vine.co/v", rpp = 6, result_type="recent", include_entities=True):
+            para = "";
+            if(sys.argc == 2)
+                para = sys.argv[1];
+            for tweet in self.api.search(q ="vine.co/v" + para, rpp = 6, result_type="recent", include_entities=True):
                 if tweet.entities != None and 'urls' in tweet.entities and \
                     len(tweet.entities['urls']) > 0 and 'expanded_url' in tweet.entities['urls'][0]:
                     vine_url = tweet.entities['urls'][0]['expanded_url']
